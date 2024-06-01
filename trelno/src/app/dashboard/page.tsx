734a14780514bd4dde67dd2ca9b02c594a7e0e1b
Ulises <ulisescappa@gmail.com>
2024-06-01 19:53:06 -0300

    Add utils/supabase folder with middleware.ts and server.ts file. Add A basic page trying to catch data from supabase in the dashboard page.

--- trelno/src/app/dashboard/page.tsx
+++ trelno/src/app/dashboard/page.tsx
@@ -1,9 +1,22 @@
-import React, { FC } from 'react';
+import { createClient } from "@/utils/supabase/server";
+import React from "react";
 
-export const Dashboard: FC = () => {
+const page = async () => {
+  const supabase = createClient();
+  const { data: boards } = await supabase.from("board").select("*");
+
+  console.log(boards);
+
   return (
     <div>
-      {/* Dashboard content */}
+      <h1>Boards</h1>
+      <ul>
+        {boards?.map((board: any) => (
+          <li key={board.id}>{board.name}</li>
+        ))}
+      </ul>
     </div>
   );
 };
+
+export default page;
